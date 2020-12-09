45efb4c3485aeeb60e632586c23f9276729e6439
Devesh Pedapudi <devesh12419@gmail.com>
2020-12-09 14:23:37 +0530

    created the model layout and initialised the parameters

---
+++ data9.m
@@ -0,0 +1,11 @@
+P=4; % Poles of motor
+R=4.98; % Stator phase resistance
+L= 5.05e-3; % stator phase inductance
+J= 15.17e-6; % Inertia
+B= 1e-6; % Damping factor
+Ke= 56.23e-6; %Back-EMF coefficient
+Tl=0.1; % Load Torque
+Va=30;
+Vb=0;
+Vc=0;
+w=1; % angular position
